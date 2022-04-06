e6e0af5d255c2ea7a8057b14b6e92c14066f66bd
Asus <Asus@192.168.1.35>
2022-04-06 12:35:54 +0530

    by kk

--- Selenium_Git_Project1/src/Demo1/Demo.java
+++ Selenium_Git_Project1/src/Demo1/Demo.java
@@ -2,6 +2,11 @@ package Demo1;
 
 public class Demo {
 
+	
+	 public void m1()
+	 {
+		 System.out.println("This is m1---Method");
+	 }
 	public static void main(String[] args) 
 	{
 	   System.out.println("Hello---Git");
