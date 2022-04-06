6e530f4d357cd6047fa1b7d0f44f5be2dc9f367f
kkk1239 <91938706+kkk1239@users.noreply.github.com>
2022-04-06 14:23:25 +0530

    DD

--- Selenium_Git_Project1/src/Demo1/Demo.java
+++ Selenium_Git_Project1/src/Demo1/Demo.java
@@ -7,6 +7,12 @@ public class Demo {
 	 {
 		 System.out.println("This is m1---Method");
 	 }
+	
+	 public void m2 ()
+	 {
+		 System.out.println("This is m1---Method");
+	 }
+	
 	public static void main(String[] args) 
 	{
 	   System.out.println("Hello---Git");
